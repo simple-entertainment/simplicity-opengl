36def11377601e453a9957db557217ee33a9f0ee
Gary Buyn <simplegsb@gmail.com>
2015-09-05 10:56:44 +1200

    Create engines using configuration json!

---
+++ src/main/c++/--simplicity-opengl-glew.h
@@ -0,0 +1,17 @@
+/*      _                 _ _      _ _
+ *     (_)               | (_)    (_) |
+ *  ___ _ _ __ ___  _ __ | |_  ___ _| |_ _   _
+ * / __| | '_ ` _ \| '_ \| | |/ __| | __| | | |
+ * \__ \ | | | | | | |_) | | | (__| | |_| |_| |
+ * |___/_|_| |_| |_| .__/|_|_|\___|_|\__|\__, |
+ *                 | |                    __/ |
+ *                 |_|                   |___/
+ *
+ * This file is part of simplicity. See the LICENSE file for the full license governing this code.
+ */
+#ifndef BOBS_ISLAND_OPENGL_GLEW_H
+#define BOBS_ISLAND_OPENGL_GLEW_H
+
+#include <GL/glew.h>
+
+#endif //BOBS_ISLAND_OPENGL_GLEW_H
